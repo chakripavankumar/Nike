72773cf7b3c5c9c4ec70dc50d74e6c99147a6e7f
pavan <siriyalapavankumar@gmail.com>
2023-11-26 15:07:31 +0530

    structure of website

---
+++ src/sections/Fotter.jsx
@@ -0,0 +1,11 @@
+
+
+const Fotter = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Fotter
---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import CustomerReviews from './CustomerReviews'
+import Fotter from './Fotter'
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffers from './SpecialOffers'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+
+export { Hero,Fotter,SpecialOffers,SuperQuality,CustomerReviews,PopularProducts,Services ,Subscribe}
